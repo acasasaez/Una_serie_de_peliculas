65482d81c248d647aa21989ac162677eec9d7a3d
acasasaez <acasasae@myuax.com>
2022-04-27 17:02:28 +0200

    hola,sigo probando

--- EJERCICIO/read_csv.py
+++ EJERCICIO/read_csv.py
@@ -1,3 +1,5 @@
+from tkinter import W
 import pandas as pd 
-df = pd(r"C:\Users\andre\OneDrive\Escritorio\Programación\Una_serie_de_peliculas\EJERCICIO\archivo.csv")
-print (df)
+df = pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/Una_serie_de_peliculas/EJERCICIO/archivo.csv")
+print(df)
+mh = pd.ExcelWriter("C:/Users/andre/OneDrive/Escritorio/Programación/Una_serie_de_peliculas/EJERCICIO/archivo.csv")
