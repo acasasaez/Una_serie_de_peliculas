cd5a763a90b9ece8e6a8085ba41d089ed854dfaa
acasasaez <acasasae@myuax.com>
2022-04-27 17:39:21 +0200

    Update e.py

--- EJERCICIO/e.py
+++ EJERCICIO/e.py
@@ -4,10 +4,12 @@ print("imprimir datos")
 print (Valoraciones)
 print (Personas)
 lista_vacia = []
+#Necesitamos sacar los datos para cálculo a partir de los datos aportados por el problema 
+#para ello creams os una función que se ajuste a los datos con los que contamos 
 def modificar_lista_vacia (a,b): 
-    for i in range (len(a)): 
-       m =  a[i]*b[i]
-       lista_vacia.append(m)
+    for i in range (len(a)):#recorremos la lista a 
+       m = a[i]*b[i]# multiplicamos los datos de la misma posición en a y b
+       lista_vacia.append(m)#los añadimos a nuestra lista vacía para conseguir la lista de datos que emplearemos finalmente
 modificar_lista_vacia (Valoraciones, Personas)
 print( lista_vacia)
 
